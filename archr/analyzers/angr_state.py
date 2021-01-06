de85af3929e4080989492d6506c7f09389ff73ff
Edward J. Schwartz <eschwartz@cert.org>
2021-01-06 10:39:08 -0500

    Add dereference arg

--- archr/analyzers/angr_state.py
+++ archr/analyzers/angr_state.py
@@ -21,11 +21,14 @@ class SimArchrMount(angr.state_plugins.filesystem.SimConcreteFilesystem):
         content = self.target.retrieve_contents(guest_path)
         return angr.SimFile(name='file://' + guest_path, content=content, size=len(content))
 
-    def _get_stat(self, guest_path):
+    def _get_stat(self, guest_path, dereference=False):
         stat_output = self.target.run_command([
-            "stat", "-c", "%n %s %b %f %u %g %D %i %h %t %T %X %Y %Z %W %o %B",
+            "stat",
+            "-c",
+            "%n %s %b %f %u %g %D %i %h %t %T %X %Y %Z %W %o %B",
             guest_path
-        ]).communicate()[0].decode().split()
+            ] + ["-L"] if dereference else []
+        ).communicate()[0].decode().split()
 
         if len(stat_output) != 17:
             return None
